59824baff6c33fff04ad8ebec7bdcfb3c76ccbc7
Gabriel Pereira de Souza <gabrielsouza97@gmail.com>
2021-03-12 17:48:27 -0300

    Initial commit with basic Spring modules

---
+++ src/main/java/com/gabrielpsz/dextrachallenge/DextraChallengeApplication.java
@@ -0,0 +1,15 @@
+package com.gabrielpsz.dextrachallenge;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.util.StringUtils;
+
+@SpringBootApplication
+public class DextraChallengeApplication {
+
+	public static void main(String[] args) {
+			SpringApplication.run(DextraChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gabrielpsz/dextrachallenge/DextraChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabrielpsz.dextrachallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DextraChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
