78a9bff6fc601c064a4af5560746457cdc58fdfc
Gabriel Pereira de Souza <gabrielsouza97@gmail.com>
2021-03-15 02:26:02 -0300

    Created all endpoints and removing keys from application.properties

--- src/main/java/com/gabrielpsz/dextrachallenge/controller/CharacterController.java
+++ src/main/java/com/gabrielpsz/dextrachallenge/controller/CharacterController.java
@@ -1,14 +1,17 @@
 package com.gabrielpsz.dextrachallenge.controller;
 
-import com.gabrielpsz.dextrachallenge.domain.MarvelCharacter;
+import com.gabrielpsz.dextrachallenge.domain.*;
 import com.gabrielpsz.dextrachallenge.service.CharacterService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequestMapping("/v1/public/characters")
 public class CharacterController {
@@ -20,8 +23,39 @@ public class CharacterController {
     }
 
     @GetMapping
-    public List<MarvelCharacter> getAllCharacters() {
-        return characterService.getAllCharacters();
+    public ResponseEntity<List<MarvelCharacter>> getAllCharacters() {
+        log.info("Request to get all characters");
+        return ResponseEntity.ok().body(characterService.getAllCharacters());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<MarvelCharacter> getCharacterById(@PathVariable String id) {
+        log.info("Request to get character with id {}", id);
+        return ResponseEntity.ok().body(characterService.getCharacterById(id));
+    }
+
+    @GetMapping("/{id}/comics")
+    public ResponseEntity<List<Comics>> getCharacterComics(@PathVariable String id) {
+        log.info("Request to get comics list from character with id {}", id);
+        return ResponseEntity.ok().body(characterService.getCharacterComics(id));
+    }
+
+    @GetMapping("/{id}/events")
+    public ResponseEntity<List<Events>> getCharacterEvents(@PathVariable String id) {
+        log.info("Request to get events list from character with id {}", id);
+        return ResponseEntity.ok().body(characterService.getCharacterEvents(id));
+    }
+
+    @GetMapping("/{id}/series")
+    public ResponseEntity<List<Series>> getCharacterSeries(@PathVariable String id) {
+        log.info("Request to get series list from character with id {}", id);
+        return ResponseEntity.ok().body(characterService.getCharacterSeries(id));
+    }
+
+    @GetMapping("/{id}/stories")
+    public ResponseEntity<List<Stories>> getCharacterStories(@PathVariable String id) {
+        log.info("Request to get stories list from character with id {}", id);
+        return ResponseEntity.ok().body(characterService.getCharacterStories(id));
     }
 
 }
--- src/main/java/com/gabrielpsz/dextrachallenge/domain/Comics.java
+++ src/main/java/com/gabrielpsz/dextrachallenge/domain/Comics.java
@@ -1,10 +1,14 @@
 package com.gabrielpsz.dextrachallenge.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Comics{
     public int available;
     public String collectionURI;
